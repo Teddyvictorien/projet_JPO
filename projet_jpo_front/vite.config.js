8de258dfdab91481170ed78edb2a9ebc71a31178
Wilhem SOREK <wsorek@myges.fr>
2022-12-27 19:16:56 +0100

    Enregistrement de l'application sous cloudflare et ajout du code permettant la connexion

--- projet_jpo_front/vite.config.js
+++ projet_jpo_front/vite.config.js
@@ -3,8 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxeYQDVuvgJab9m5lleCPbo4pMe9nifx8",
+    authDomain: "projet-jpo.firebaseapp.com",
+    projectId: "projet-jpo",
+    storageBucket: "projet-jpo.appspot.com",
+    messagingSenderId: "609979799008",
+    appId: "1:609979799008:web:4c0dbe57bd5a9b78e5c785",
+    measurementId: "G-6NKMFYTK1Z"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
